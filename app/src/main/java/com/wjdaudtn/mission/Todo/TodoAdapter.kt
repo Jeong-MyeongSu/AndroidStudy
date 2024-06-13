f51cd9c5b37704412b35b76f63ead49675272e1a
jeong-myeong-su <wjdaudtntn@naver.com>
2024-06-13 18:34:48 +0900

    🍻 beers:

--- app/src/main/java/com/wjdaudtn/mission/Todo/TodoAdapter.kt
+++ app/src/main/java/com/wjdaudtn/mission/Todo/TodoAdapter.kt
@@ -4,6 +4,8 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.wjdaudtn.mission.databinding.ItemMainBinding
 
+class TodoViewHolder(val binding: ItemMainBinding) : RecyclerView.ViewHolder(binding.root)
+
 //class TodoAdapter(val binding: ItemMainBinding):
 //        RecyclerView.Adapter<RecyclerView.ViewHolder>(){
 //    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
