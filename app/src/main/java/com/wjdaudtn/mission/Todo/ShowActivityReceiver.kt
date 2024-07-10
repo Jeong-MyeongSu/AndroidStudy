b282ffd3edb42cbed372392847d6826642dbe0d5
jeong-myeong-su <jmsnzz@gmail.com>
2024-07-10 16:51:09 +0900

    🚀 Add setContentIntent to TodoMainActivity from AlarmNotification
    알람 노티피케이션 클릭시 투두메인엑티비티 띄우는것 추가

---
+++ app/src/main/java/com/wjdaudtn/mission/Todo/ShowActivityReceiver.kt
@@ -0,0 +1,12 @@
+package com.wjdaudtn.mission.todo
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class ShowActivityReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+
+    }
+}
